9d21d2b5ac16f6f84fc2a6ff41d50b9458cd911b
alhyderroman <alhyderroman807@yahoo.com>
2024-06-01 07:21:32 +0600

    mmessage

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,31 @@
+// import { initializeApp } from 'firebase/app'
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// }
+
+// export const app = initializeApp(firebaseConfig)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
+
+
